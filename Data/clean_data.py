34fcc3cafdba8a977b135b750723623ee89f2b94
kiwiblaster <frankkev@students.zhaw.ch>
2024-04-01 22:09:56 +0200

    add log returns

--- Data/clean_data.py
+++ Data/clean_data.py
@@ -7,6 +7,7 @@ Created on Mon Apr  1 20:08:36 2024
 """
 
 import pandas as pd
+import numpy as np
 
 master = pd.read_excel('master.xlsx')
 price = pd.read_excel('instruments_price.xlsx')
@@ -25,7 +26,13 @@ def filter_dataframe_by_date_range(df, target_date,days_before,days_after):
     end_date = target_date + pd.Timedelta(days=days_after)
     filtered_df = df.loc[start_date:end_date]
     filtered_df = filtered_df.fillna(method='ffill').fillna(method='bfill')
+    
     return filtered_df
 
 
 filtered_dataframe = filter_dataframe_by_date_range(price_event1, '2010-04-15',30,30)
+
+
+log_returns = np.log(filtered_dataframe / filtered_dataframe.shift(1))
+
+log_returns = log_returns.drop(log_returns.index[0])
